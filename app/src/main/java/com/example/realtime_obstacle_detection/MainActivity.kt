107726b6923c20b5ccc7a996ee53c1f1388813ca
Abtinz <abtinzandi@gmail.com>
2024-10-17 14:08:45 +0330

    MODEL CONNECTION PART -4 connected with model lets test

--- app/src/main/java/com/example/realtime_obstacle_detection/MainActivity.kt
+++ app/src/main/java/com/example/realtime_obstacle_detection/MainActivity.kt
@@ -1,26 +1,110 @@
 package com.example.realtime_obstacle_detection
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import com.example.realtime_obstacle_detection.presentation.tensorflow.TensorFlowLiteFrameAnalyzer
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.realtime_obstacle_detection.data.TensorFlowLiteClassifier
+import com.example.realtime_obstacle_detection.domain.ClassificationResults
+import com.example.realtime_obstacle_detection.presentation.camera.CameraPreview
 import com.example.realtime_obstacle_detection.ui.theme.Realtime_Obstacle_DetectionTheme
+import com.example.realtime_obstacle_detection.ui.theme.primary
+import com.example.realtime_obstacle_detection.ui.theme.secondary
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        if(!hasCameraPermission()) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA), 0
+            )
+        }
         setContent {
             Realtime_Obstacle_DetectionTheme {
 
+                var classifications by remember {
+                    mutableStateOf(emptyList<ClassificationResults>())
+                }
+
+                val analyzer = remember {
+                    TensorFlowLiteFrameAnalyzer(
+                        classifier = TensorFlowLiteClassifier(
+                            context = applicationContext
+                        ),
+                        onResults = {
+                            classifications = it
+                        }
+                    )
+                }
+
+                val controller = remember {
+                    LifecycleCameraController(applicationContext).apply {
+                        setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
+                        setImageAnalysisAnalyzer(
+                            ContextCompat.getMainExecutor(applicationContext),
+                            analyzer
+                        )
+                    }
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ){
+
+                    //we need a camera preview as background
+                    CameraPreview(
+                        controller = controller,
+                        modifier = Modifier.fillMaxSize()
+                    )
+
+                    //our final results will be attached to page header
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.TopCenter)
+                    ) {
+                        classifications.forEach {
+                            Text(
+                                text = it.label,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(secondary)
+                                    .padding(10.dp),
+                                textAlign = TextAlign.Center,
+                                fontSize = 20.sp,
+                                color = primary
+                            )
+                        }
+                    }
+                }
             }
         }
     }
+
+    private fun hasCameraPermission() = ContextCompat.checkSelfPermission(
+        this, Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
 }
