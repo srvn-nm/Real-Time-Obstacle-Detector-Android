14a27244a118265b28bb7cac11b5228d66e4b502
Abtinz <abtinzandi@gmail.com>
2024-10-17 14:00:41 +0330

    MODEL CONNECTION PART -2

---
+++ app/src/main/java/com/example/realtime_obstacle_detection/data/TensorFlowLiteClassifier.kt
@@ -0,0 +1,88 @@
+package com.android.landmarkdetection.data
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.view.Surface
+import com.android.landmarkdetection.domain.ClassificationResults
+import com.android.landmarkdetection.domain.LandmarkClassifier
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
+
+class TensorFlowLiteClassifier(
+    private val context: Context,
+    private val conf: Float = 0.5f,
+    private val count: Int = 1,
+    private val threads: Int = 2,
+) :LandmarkClassifier{
+
+    private var classifier: ImageClassifier? = null
+
+    private fun setupClassifier() {
+
+        //basic options like threads counts or gpu usage
+        val baseOptions = BaseOptions.builder()
+            .setNumThreads(threads)
+            .build()
+
+        //other options of an image classifier like threads setMaxResults or setScoreThreshold (we have to set baseOptions here too)
+        val options = ImageClassifier.ImageClassifierOptions.builder()
+            .setBaseOptions(baseOptions)
+            .setMaxResults(count)
+            .setScoreThreshold(conf)
+            .build()
+
+        val MODEL_PATH = "best_float32.tflite"
+        try {
+            classifier = ImageClassifier.createFromFileAndOptions(
+                context,
+                MODEL_PATH,
+                options
+            )
+        } catch (error: IllegalStateException) {
+            error.printStackTrace()
+        }
+    }
+
+    override fun getOrientationFromRotation(rotation: Int): ImageProcessingOptions.Orientation {
+        return when(rotation) {
+            Surface.ROTATION_270 -> ImageProcessingOptions.Orientation.BOTTOM_RIGHT
+            Surface.ROTATION_90 -> ImageProcessingOptions.Orientation.TOP_LEFT
+            Surface.ROTATION_180 -> ImageProcessingOptions.Orientation.RIGHT_BOTTOM
+            else -> ImageProcessingOptions.Orientation.RIGHT_TOP
+        }
+    }
+
+    override fun classify(bitmap: Bitmap, rotation: Int): List<ClassificationResults> {
+
+        //initialization of classifier
+        if(classifier == null) {
+            setupClassifier()
+        }
+
+        //we need to process our bitmap as TensorImage for preparation to model input
+        val imageProcessor = ImageProcessor.Builder().build()
+        val image = imageProcessor.process(TensorImage.fromBitmap(bitmap))
+
+        val imageProcessingOptions = ImageProcessingOptions.builder()
+            .setOrientation(getOrientationFromRotation(rotation))
+            .build()
+
+        val results = classifier?.classify(image, imageProcessingOptions)
+
+        //now we have to map our return type to our returned data class
+        return results?.flatMap { classifications ->
+            classifications.categories.map { category ->
+                ClassificationResults(
+                    label = category.displayName,
+                    score = category.score
+                )
+            }
+        }?.distinctBy { it.label } ?: emptyList() //no result for picture
+    }
+
+
+
+}
