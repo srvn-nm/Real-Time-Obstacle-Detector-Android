d81f257e2864a7e6ec8b79be4fef15fe9afbfd81
Abtinz <abtinzandi@gmail.com>
2024-10-21 23:48:33 +0330

    new interface

--- app/src/main/java/com/example/realtime_obstacle_detection/domain/ObstacleClassifier.kt
+++ app/src/main/java/com/example/realtime_obstacle_detection/domain/ObstacleClassifier.kt
@@ -1,18 +1,18 @@
 package com.example.realtime_obstacle_detection.domain
 
 import android.graphics.Bitmap
-import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
 
 interface ObstacleClassifier {
+
+    fun onEmptyDetect()
 
     /**
      * Classifies the given bitmap image to identify obstacle.
      *
-     * @param bitmap The bitmap image to be processed from the camera view.
-     * @param rotation The rotation angle of the image which needs to be corrected before processing.
-     * @return A list of classification results, each containing a label and a confidence score.
+     * @param detectedScene The bitmap image to be processed from the camera view.
+     * @param objectDetectionResults A list of classification results, each containing a label and a confidence score.
      */
-    fun classify(bitmap: Bitmap, rotation:Int):List<ClassificationResults>
+    fun onDetect(objectDetectionResults: List<ObjectDetectionResult>, detectedScene: Bitmap)
 
-    fun getOrientationFromRotation(rotation: Int): ImageProcessingOptions.Orientation
+    //fun getOrientationFromRotation(rotation: Int): ImageProcessingOptions.Orientation
 }
